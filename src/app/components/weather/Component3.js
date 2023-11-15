8585c00925a327189b7146a38bb4be6fd924254f
gencery <genceryigiter@gmail.com>
2023-11-15 11:26:43 +0300

    .

--- src/app/components/weather/Component3.js
+++ src/app/components/weather/Component3.js
@@ -1,18 +1,18 @@
-import { useRef, useState } from "react";
-import { flushSync } from "react-dom";
+"use client";
 import { objToArray } from "@/app/utility/functions";
 import WeatherIcon from "../WeatherIcon";
+import { useState } from "react";
 
 function Week({ daysOfWeek }) {
 	return (
-		<ul className="flex">
+		<ul className="flex justify-between">
 			{objToArray(daysOfWeek).map((day) => {
 				return (
 					<li
 						key={day.key}
-						className="text-white list-none flex flex-col items-center space-y-2 w-[70px] sm:w-[100px]">
+						className="text-white list-none flex flex-col items-center space-y-2 m-0.25">
 						<p className="uppercase text-[8px] md:text-[16px]">{day["key"]}</p>
-						<div className="w-[30px] h-[25px] md:w-[60px] md:h-[50px] relative">
+						<div className="w-[35px] h-[25px] md:w-[60px] md:h-[50px] relative">
 							<WeatherIcon weatherType={day.type} size={"big"} />
 						</div>
 						<p className="text-[14px] md:text-[16px]">
@@ -26,53 +26,50 @@ function Week({ daysOfWeek }) {
 	);
 }
 
-//Gallery kaynak kodu: `https://react.dev/learn/manipulating-the-dom-with-refs`
-// function Gallery({ itemList }) {
-// 	const selectedRef = useRef(null);
-// 	const [index, setIndex] = useState(0);
+function Gallery({ children }) {
+	const [index, setIndex] = useState(1);
 
-// 	return (
-// 		<>
-// 			<nav>
-// 				<button
-// 					onClick={() => {
-// 						flushSync(() => {
-// 							if (index < itemList.length - 1) {
-// 								setIndex(index + 1);
-// 							} else {
-// 								setIndex(0);
-// 							}
-// 						});
-// 						selectedRef.current.scrollIntoView({
-// 							behavior: "smooth",
-// 							block: "nearest",
-// 							inline: "center",
-// 						});
-// 					}}>
-// 					Next
-// 				</button>
-// 			</nav>
-// 			<div>
-// 				<ul>
-// 					{itemList.map((item, i) => (
-// 						<li key={item.id} ref={index === i ? selectedRef : null}>
-// 							<img
-// 								className={index === i ? "active" : ""}
-// 								src={item.imageUrl}
-// 								alt={"Cat #" + item.id}
-// 							/>
-// 						</li>
-// 					))}
-// 				</ul>
-// 			</div>
-// 		</>
-// 	);
-// }
+	function handlePrevSlide() {
+		if (index == 0) {
+			setIndex(children.length - 1);
+		} else {
+			setIndex(index - 1);
+		}
+	}
+	function handleNextSlide() {
+		if (index == children.length - 1) {
+			setIndex(0);
+		} else {
+			setIndex(index + 1);
+		}
+	}
+
+	let weeks = ["Previous Week", "Current Week", "Next Week"];
+
+	return (
+		<div className="text-white">
+			<div className="flex">
+				<button className="mr-1" onClick={handlePrevSlide}>
+					{"❮"}
+				</button>
+				<div className="grow">{children[index]}</div>
+				<button className="ml-1" onClick={handleNextSlide}>
+					{"❯"}
+				</button>
+			</div>
+			<p className="text-center my-2">{weeks[index]}</p>
+		</div>
+	);
+}
 
 export default function Component3({ weeklyWeather }) {
 	return (
 		<div className="box ml-0 xl:ml-4 my-4 p-4 space-y-2">
-			<Week daysOfWeek={weeklyWeather.previous} />
+			<Gallery>
+				<Week daysOfWeek={weeklyWeather.previous} />
+				<Week daysOfWeek={weeklyWeather.current} />
+				<Week daysOfWeek={weeklyWeather.next} />
+			</Gallery>
 		</div>
 	);
 }
